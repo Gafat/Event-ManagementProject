29eb7d4d46b618dc11abfcb417f73d350936af47
Gherppo <gezahegn.herppo@gmail.com>
2015-03-06 22:03:04 -0600

    reservation controller modified

--- EventManagement/src/gafat/controller/ReservationController.java
+++ EventManagement/src/gafat/controller/ReservationController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ReservationController {
+	//update
 	
 @RequestMapping(value="/gez" ,method=RequestMethod.GET)
 
